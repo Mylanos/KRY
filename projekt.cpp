9cbaaaa5438e6057dd31a9f06db2f4e11c8b2ad3
Mylanos <marekziska99@gmail.com>
2023-03-07 21:08:03 +0100

    edits

--- projekt.cpp
+++ projekt.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include "cxxopts.hpp"
 
+int LIMIT_ABECEDA = 26;
+
 class Specification {         // The class
   public:                     // Access specifier
     int a;                    // first key 'a' (int variable)
@@ -29,6 +31,35 @@ class Specification {         // The class
     }
 };
 
+//void read_file(std::string file_path)
+//{
+// std::ifstream myfile;
+//  myfile.open ("example.txt");
+// myfile << "Writing this to a file.\n";
+//  myfile.close();
+//}
+//
+// source and credit to https://www.javatpoint.com/prime-number-program-in-cpp
+void check_prime_number(int number)
+{
+  if(number > LIMIT_ABECEDA){
+    std::cerr << "Error parsing options: given key was out of range for given abeceda" << std::endl;
+    exit(1);
+  }
+  int n, i, m=0;  
+  m=number/2;  
+
+  for(i = 2; i <= m; i++)  
+  {  
+      if(n % i == 0)  
+      {   
+          std::cerr << "Error parsing options: given key was not a prime number" << std::endl;
+          exit(1);
+          std::cout << "test";
+      }  
+       std::cout << "test " << n;
+  }   
+}
 cxxopts::ParseResult
 parse(int argc, char* argv[])
 {
@@ -46,8 +77,8 @@ parse(int argc, char* argv[])
       ("e", "sifrovani", cxxopts::value<bool>(spec.encryption))
       ("d", "desifrovani", cxxopts::value<bool>(spec.decryption))
       ("c", "desifrovani bez znalosti klice", cxxopts::value<bool>(spec.decryption_no_key))
-      ("a", "prvni klic", cxxopts::value<int>(spec.a), "N")
-      ("b", "druhy klic", cxxopts::value<int>(spec.b), "N")
+      ("a", "prvni klic", cxxopts::value<int>(spec.a), "a muze byt pouze prvocislo mensi nez delka abecedy 26.")
+      ("b", "druhy klic", cxxopts::value<int>(spec.b), "b muze byt pouze prvocislo mensi nez delka abecedy 26.")
       ("f", "cesta k souboru", cxxopts::value<std::string>(spec.input_file), "IN_FILE")
       ("o", "vystupni soubor s otevrenym textem", cxxopts::value<std::string>(spec.output_file), "OUT_FILE")
       ("h,help", "Print help")
@@ -94,11 +125,13 @@ parse(int argc, char* argv[])
 
     if (result.count("a"))
     {
+      check_prime_number(spec.a);
       std::cout << "a = " << spec.a << std::endl;
     }
 
      if (result.count("b"))
     {
+      check_prime_number(spec.b);
       std::cout << "b = " << spec.b << std::endl;
     }
 
@@ -107,7 +140,7 @@ parse(int argc, char* argv[])
 
   } catch (const cxxopts::OptionException& e)
   {
-    std::cout << "error parsing options: " << e.what() << std::endl;
+    std::cerr << "error parsing options: " << e.what() << std::endl;
     exit(1);
   }
 }
